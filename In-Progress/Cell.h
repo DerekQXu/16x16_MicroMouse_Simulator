c90ce744c2404ec8c3172dab86581bd837f73823
Derek Xu <derekqxu@gmail.com>
2017-08-27 14:17:51 -0700

    Update Cell.h

--- In-Progress/Cell.h
+++ In-Progress/Cell.h
@@ -7,20 +7,13 @@ class Cell
 public:
     Cell();
     void setWall(Wall* wall, Direction ID);
-    Wall* getWall(Direction ID);
-    void setCell(Cell* cell, Direction ID);
-    Cell* getCell(Direction ID);
+    bool getWall(Direction ID);
 
 private:
     Wall* m_leftWall;
     Wall* m_rightWall;
     Wall* m_upWall;
     Wall* m_downWall;
-
-    Cell* m_leftCell;
-    Cell* m_rightCell;
-    Cell* m_upCell;
-    Cell* m_downCell;
 };
 
 #endif // CELL_H
